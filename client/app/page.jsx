15e38fc8d68225bc86d45947617f4a359938f204
guptarohan6502 <rohangupta6502@gmail.com>
2024-02-22 03:53:41 +0530

    commit 9

---
+++ client/app/page.jsx
@@ -0,0 +1,32 @@
+import Bottom from '@components/bottom';
+
+const Home = () => {
+  return (
+    <section id="main">
+      <section className="showcase">
+        <div className="video-container">
+          <video
+            src="/Yearbook_portal_full.mp4"
+            autoPlay
+            muted
+            loop
+            playsInline
+          ></video>
+        </div>
+        <div className="content">
+          <h1>SACC</h1>
+          <h2>Presents</h2>
+          <h2>YEARBOOK</h2>
+          <h3>For 2k24 Graduates Only</h3>
+          <a href="/login" className="btn">
+            Login with CAS
+           
+          </a>
+        </div>
+      </section>
+      <Bottom />
+    </section>
+  );
+};
+
+export default Home;
---
+++ client/components/bottom.jsx
@@ -0,0 +1,23 @@
+
+const Bottom = () => {
+  return (
+    
+	<section id="about">
+		<h1>About SACC</h1>
+		<p>
+			Student Alumni Connect Cell (SACC) is a student-run organization that aims to foster connections between students, alumni, and the Institute. SACC provides mentorship, organizes events, and offers resources to help students and alumni engage with their alma mater and fellow alumni.
+		</p>
+
+		<h2>Follow Us On Social Media</h2>
+
+		<div class="social">
+		<a href="https://www.instagram.com/alumnicell_iiith/" target="_blank"><i class="fab fa-instagram fa-3x"></i></a>
+			<a href="https://www.facebook.com/iiith.alumnicell" target="_blank"><i class="fab fa-facebook fa-3x"></i></a>
+			<a href="https://www.linkedin.com/company/alumni-cell-iiit-h/" target="_blank"><i class="fab fa-linkedin fa-3x"></i></a>
+		</div>
+	</section>
+
+  )
+}
+
+export default Bottom
