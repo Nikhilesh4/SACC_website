6316d37a9298f4e6ff75dadadaef2087291d86ef
Bhav Beri <bhavberi@gmail.com>
2024-03-10 10:46:05 +0530

    Added server deploy to nginx command

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,5 @@
 
 echo "Please setup the environment variables in the .env file"
 docker compose -p sacc -f docker-compose.prod.yml up --build -d
+
+../proxy_container.sh -c sacc-nginx-1 -d sacc.iiit.ac.in -p 80
