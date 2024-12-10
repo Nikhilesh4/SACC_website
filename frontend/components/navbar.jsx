9775f5b5364a1d5d49e81b2f3a32ca069d6abab1
avi1o1 <aviral.gupta@research.iiit.ac.in>
2024-12-10 16:43:41 +0530

    Update NavBar

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -1,110 +1,55 @@
 "use client";
-// @client
-import React from "react";
-import { useState, useEffect } from "react";
-import { Nav, Navbar, NavDropdown } from "react-bootstrap";
-import Fade from "react-awesome-reveal";
-import Button from "react-bootstrap/Button";
-// import Fade from './fade.jsx';
-// import '@styles/navbar.scss';
+import React, { useState, useEffect } from "react";
+import { Nav, Navbar } from "react-bootstrap";
 import "@styles/navbar.scss";
+
 const NavbarComponent = () => {
   const [authenticated, setAuthenticated] = useState(false);
 
   useEffect(() => {
-    var arrayb = document.cookie;
-    arrayb = arrayb.split(";");
-    // console.log(arrayb);
-    for (const item of arrayb) {
-      // console.log(item);
-      if (item.includes("Authorization_YearBook")) {
+    const cookies = document.cookie.split(";");
+    for (const cookie of cookies) {
+      if (cookie.includes("Authorization_YearBook")) {
         setAuthenticated(true);
       }
     }
   }, []);
 
   const changeBackground = () => {
-    let header = document.getElementsByClassName("header-nav")[0];
-    if (window.scrollY < 200) {
+    const header = document.getElementsByClassName("header-nav")[0];
+    if (window.scrollY < 100) {
       header.classList.remove("sticky");
     } else {
       header.classList.add("sticky");
     }
   };
 
-  // Add event listener only if window is defined
   if (typeof window !== "undefined") {
     window.addEventListener("scroll", changeBackground);
   }
 
   return (
-    <>
-      <Navbar
-        className="header-nav"
-        fixed="top"
-        collapseOnSelect
-        bg="dark"
-        expand="lg"
-      >
-        <Fade cascade>
-          {/* <Navbar.Brand href="." className="justify-content-center"> */}
-          {/* <img src="./assets/images/SACC_logo.png" alt="SACC" width={50} height={50} style={{ marginLeft: "5rem" }} /> */}
-          {/* </Navbar.Brand> */}
-        </Fade>
-        <Navbar
-          id="responsive-navbar-nav"
-          className="justify-content-center m-auto "
-          //  style={{ marginRight: "9rem" }}
-        >
-          <Nav className="mr-auto">
-            <Fade cascade damping={0.5}>
-              <Nav.Link className="px-3" href="#about">
-                About Us
-              </Nav.Link>
-              {/* <Nav.Link className="px-3" href="#education">
-              Education
-            </Nav.Link> */}
-            </Fade>
-          </Nav>
-
-          <Fade cascade>
-            <Navbar.Brand href="." className="brand-name mx-auto">
-              SACC
-            </Navbar.Brand>
-          </Fade>
-
-          <Nav className="mr-auto">
-            <Fade cascade damping={0.5}>
-              {authenticated ? (
-                <Nav.Link className="px-3" href="/yearbook">
-                  Yearbook
-                </Nav.Link>
-              ) : (
-                <Nav.Link className="px-3" href="/api/login">
-                  Login
-                </Nav.Link>
-              )}
-              {/* <Nav.Link className="px-3" href="#project">
-              Projects
-            </Nav.Link> */}
-            </Fade>
-          </Nav>
-          {authenticated && (
-            // <Button className="my-auto" href="#" variant="dark" active>
-              <a
-                target="_blank"
-                href="../../Yearbook_2019.pdf"
-                download="yearbook_2019.pdf"
-                className="text-white"
-              >
-                <Button className="my-auto"> Get PDF</Button>
-               
-              </a>
-            // </Button>
-          )}
-        </Navbar>
-      </Navbar>
-    </>
+    <Navbar className="header-nav" fixed="top" bg="dark" expand="lg">
+      <Navbar.Brand href="/">
+        <img
+          src="/assets/images/collegeLogo.png"
+          alt="Logo"
+          className="logo"
+          style={{ width: '169px', height: '50px' }}
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-center">
+        <Nav className="mx-auto">
+          <Nav.Link className="px-3 text-white" href="/">Home</Nav.Link>
+          <Nav.Link className="px-3 text-white" href="/team">Team</Nav.Link>
+          <Nav.Link className="px-3 text-white active" href={authenticated ? "/yearbooks" : "/api/login"}>Yearbooks</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link className="px-3 text-white loginButton" href="/api/login">Login</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
