ab00e48fe3109b9f12f30484dca5c05e315436cb
avi1o1 <aviral.gupta@research.iiit.ac.in>
2025-01-07 00:22:08 +0530

    Cleanse yearbook file

--- run.sh
+++ run.sh
@@ -4,12 +4,6 @@ cd frontend
 npm install
 cd ..
 
-<<<<<<< HEAD
-docker-compose up --build
-
-docker-compose down
-=======
 sudo docker-compose up --build
 
 sudo docker-compose down
->>>>>>> 53c082e85679e1932cd655f84073369f0aa0868a
